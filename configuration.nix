9efc4afe374ed4d2572d95825fb350833714b7e7
Chris Samuelson <chris.sam55@gmail.com>
2025-04-11 21:37:31 -0400

    Set-up function wsl only configuration

--- configuration.nix
+++ configuration.nix
@@ -1,71 +1,30 @@
-# Profile baselines/examples, including a true minimum; this minimum is probably broken
-# https://github.com/NixOS/nixpkgs/tree/master/nixos/modules/profiles
-
-# Plausibly the most minimal installation available
-#{ config, pkgs, ... }: {
-#  imports = [
-#  # Include the results of the hardware scan.
-#    ./hardware-configuration.nix
-#  ];
-
-#  boot.loader.grub.device = "/dev/sda";   # (for BIOS systems only)
-#  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
-
-#  # Enable the OpenSSH server.
-#  services.sshd.enable = true;
-#}
-
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }: {
-  imports = [
-    ./hardware-configuration.nix
-    <home-manager/nixos>
-  ];
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  boot.loader.grub = {
+  wsl = {
     enable = true;
-    device = "/dev/nvme0n1";
-    useOSProber = true;
+    #nativeSystemd = 
+    defaultUser = "chris";
+    interop.includePath = true;
+    wslConf = {
+      boot.systemd = true;
+      automount = {
+        options = "metadata,uid=1000,gid=1000";
+        root = "/mnt";
+      };
+    };
+    useWindowsDriver = true;
+    usbip.enable = true;
   };
 
-  system.copySystemConfiguration = true;
+  boot.loader.grub.enable = false;
 
   networking = {
     networkmanager.enable = true;
 
-    hostName = "chris-laptap-ava";
-    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # Configure network proxy if necessary
-    # proxy.default = "http://user:password@proxy:port/";
-    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Open ports in the firewall.
-    #   53317 is for localsend
-    firewall.allowedTCPPorts = [ 53317 ];
-    # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
+    hostName = "chris-desktop-wsl";
   };
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd = {
-    services = {
-      "getty@tty1".enable = false;
-      "autovt@tty1".enable = false;
-    };
-
-    # changed for AFL fuzzer
-    coredump.enable = false;
-  };
-
-  # changed for AFL fuzzer; might want it anyway...
-  powerManagement.cpuFreqGovernor = "performance";
 
   # Set your time zone.
   time.timeZone = "America/New_York";
@@ -92,40 +51,6 @@
     "flakes"
   ];
 
-  services = {
-    xserver = {
-      enable = true;
-
-      # Enable the GNOME Desktop Environment.
-      desktopManager.gnome.enable = true;
-
-      displayManager = {
-        gdm.enable = true;
-
-        # Enable automatic login
-	autoLogin = {
-          enable = true;
-          user = "chris";
-	};
-      };
-
-      # Configure keymap in X11
-      layout = "us";
-      xkbVariant = "dvp";
-
-      # Enable touchpad support (enabled default in most desktopManager).
-      # libinput.enable = true;
-    };
-
-    # Enable CUPS to print documents.
-    printing.enable = true;
-
-    # Enable the OpenSSH daemon.
-    # openssh.enable = true;
-  };
-
-  # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -133,12 +58,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   virtualisation.docker = {
@@ -149,9 +68,10 @@
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.chris = {
     isNormalUser = true;
+    uid = 1000;
+    #primaryGroup = "chris";
     description = " Chris Samuelson";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
@@ -159,21 +79,14 @@
     ];
   };
 
-  # To setup home-manager as a nixos module do this:
-  #home-manager.useGlobalPkgs = true;
-  #home-manager.useUserPackages = true;
-  #home-manager.users.chris = import ./home.nix;
-  # This requires root to update user packages since `nixos-rebuild switch` is
-  #  required to get changes to the home manager configuration
-  # as-is, I'm using nixos to install home manager, but not manage the configuration;
-  #  i.e. a standalone installation of home-manager
+  users.groups.chris = {
+    gid = 1000;
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # ensure at least a text editor is always available to update this config :)
-    git # flakes
-    wget # to download anything to help get things setup for a fresh system
+    vim
+    git # for flakes
+    wget
   ];
 
   programs = {
@@ -186,32 +99,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # this builds, but doesn't function.......
-  # fileSystems."/mnt" = {
-  #   device = "https://truenas.domain.dev";
-  #   fsType = "cifs";
-  #   options = [
-  #     "username=..."
-  #     "password=..."
-  #     "x-system.automount"
-  #     "noauto"
-  #   ];
-  # };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 }
 
--- flake.nix
+++ flake.nix
@@ -1,26 +1,47 @@
-# https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
-# https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-with-flakes-enabled
-# https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-configuration-explained
 
+#packages.x86_64-linux.nixosConfigurations."wsl".config.system.build.nixos-rebuild
+#legacyPackages.x86_64-linux.nixosConfigurations."wsl".config.system.build.nixos-rebuild
+#nixosConfigurations."wsl".config.system.build.nixos-rebuild
 {
-  description = "A very basic flake";
+  description = "";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    #flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
   };
 
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.chris-laptop-ava = nixpkgs.lib.nixosSystem {
-      system = "x86_64-Linux";
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      #pkgs = import nixpkgs { inherit system; };
+    in {
+      # Define your NixOS configurations by hostname
+      nixosConfigurations = {
+        wsl = nixpkgs.lib.nixosSystem {
+          system = system;
 
-      specialArgs = attrs;
-      modules = [
-        ./configuration.nix
-      ];
+          # pull in custom configuration files.
+          modules = [
+	    nixos-wsl.nixosModules.wsl
+            ./configuration.nix
+          ];
+
+          # Pass additional inputs to configuration.
+          specialArgs = {
+            inherit system;
+            # Making other inputs available for deeper configuration files
+            inputs = {
+              nixpkgs = nixpkgs;
+              home-manager = home-manager;
+            };
+          };
+        };
+      };
     };
-  };
 }
+
