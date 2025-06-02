c5e6b8933f8da1d59ef7fbc5b0848cbb26e2139b
Chris Samuelson <chris.sam55@gmail.com>
2025-06-01 22:22:49 -0400

    Trying to integrate init-bash..

--- users/chris/home.nix
+++ users/chris/home.nix
@@ -7,9 +7,19 @@ let
     chmod +x $out/my-script
   '';
 
+  # add dep to makeWrapper some how?
+  #nativeBuildInputs = [ pkgs.makeWrapper ];
   fullWrapped = pkgs.runCommand "wrapped-apps" {} ''
     mkdir -p $out/bin
     cp -r ${init-bash.packages.${system}.default}/bin/* $out/bin
+    cp -r ${init-bash.packages.${system}.default}/bin/.init.bash-wrapped $out/bin
+    rm $out/bin/init.bash
+    mv $out/bin/.init.bash-wrapped $out/bin/init.bash
+
+    # re-wrap the original
+    # maybe the flake doesn't need to wrap to begin with?
+    wrapProgram $out/bin/init.bash \
+      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bash ]}
 
     mkdir -p $out/bin/apps
     cp -r ${userScripts}/* $out/bin/apps
