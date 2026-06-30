273a075aa2b8bca2c5abaf2c94163e7d3b6affda
Chris Samuelson <chris.sam55@gmail.com>
2026-06-30 08:59:32 -0400

    Fixed config
    no more error messages at start :)

--- users/chris/modules/neovim/config/lua/plugins/lsp-config.lua
+++ users/chris/modules/neovim/config/lua/plugins/lsp-config.lua
@@ -8,18 +8,15 @@ return {
     config = function()
       require("mason").setup()
       local mlsp = require("mason-lspconfig")
-
       local desired = {
         "lua_ls",
         "bashls",
         "clangd",
         "cmake"
       }
-
       local available = vim.tbl_filter(function(s)
         return vim.tbl_contains(mlsp.get_available_servers(), s)
       end, desired)
-
       local missing = vim.tbl_filter(function(s)
         return not vim.tbl_contains(available, s)
       end, desired)
@@ -29,34 +26,37 @@ return {
       end
 
       mlsp.setup({
-        ensure_installed = available
-      })
-
-      local lspconfig = require("lspconfig")
-
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            telemetry = {
-              enable = false
-            },
-          }
+        ensure_installed = available,
+        handlers = {
+          function(server_name)
+            vim.lsp.config(server_name, {})
+          end,
+          lua_ls = function()
+            vim.lsp.config("lua_ls", {
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { 'vim' },
+                  },
+                  workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                  },
+                  telemetry = {
+                    enable = false
+                  },
+                }
+              }
+            })
+          end,
+          clangd = function()
+            vim.lsp.config("clangd", {
+              cmd = { "clangd", "--background-index", "--clang-tidy" },
+              filetypes = { "c", "cpp", "cc", "h", "hh", "hpp", "objc", "objcpp" },
+              root_markers = { "compile_commands.json", ".git" },
+            })
+          end,
         }
-      })
-
-      lspconfig.clangd.setup({
-        -- cmd = { "clangd", "--clang-tidy" },
-        -- cmd = { "clangd", "--compile-commands-dir=build", "--clang-tidy" },
-        cmd = { "clangd", "--background-index", "--clang-tidy" },
-        filetypes = { "c", "cpp", "cc", "h", "hh", "hpp", "objc", "objcpp" },
-        root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
       })
 
       vim.keymap.set('n', '<leader>d', ':Telescope diagnostics<CR>', {})
--- users/chris/modules/neovim/config/lua/plugins/treesitter.lua
+++ users/chris/modules/neovim/config/lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
+    require("nvim-treesitter.config").setup({
       ensure_installed = { "lua", "python", "cpp" },
-      highlight = { enable = true },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
       indent = { enable = true },
     })
   end
